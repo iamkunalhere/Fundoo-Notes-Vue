f282b5f39f49d1e29a6045eec7d82e9193fa6ffa
iamkunalhere <iamkunalhere@gmail.com>
2020-05-28 14:28:40 +0530

    ADDED: Component for Registration

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <div class="container">
+    <div class="fields">
+      <input type="text" placeholder="Firstname" />
+      <input type="text" placeholder="Lastname" />
+      <input type="text" placeholder="Email" />
+      <input type="text" placeholder="Password" />
+      <button>Register</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "Registration",
+  props: {
+    msg: String
+  }
+};
 </script>
-
-<style>
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+}
 </style>
